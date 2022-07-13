e446451b4d6a87ba0f83fe73e08143e52496a67a
FabioDevCode <0677384849@orange.fr>
2022-07-13 23:22:18 +0200

    Add animation list

--- src/App.vue
+++ src/App.vue
@@ -12,14 +12,14 @@
             <button @click="addToList()">ADD</button>
         </form>
 
-        <ul id="list">
+        <transition-group name="bounce" tag="ul" id="list">
             <li :key="list.indexOf(prop)" v-for="prop in list" class="proposition">
                 <p> {{ prop }} </p>
                 <button class="close" @click="deleteElementList(prop)">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z"/></svg>
                 </button>
             </li>
-        </ul>
+        </transition-group>
 
         <button class="reset_list" v-if="list.length > 1" @click="resetList()">
             Clear list
@@ -442,6 +442,24 @@
 
     .animate2 {
         animation: animChoicer2 2s ease-in-out;
+    }
+
+    .bounce-enter-active {
+        animation: bounce-in .5s;
+    }
+    .bounce-leave-active {
+        animation: bounce-in .5s reverse;
+    }
+    @keyframes bounce-in {
+        0% {
+            transform: scale(0);
+        }
+        50% {
+            transform: scale(1.1);
+        }
+        100% {
+            transform: scale(1);
+        }
     }
 
     @keyframes animChoicer {
