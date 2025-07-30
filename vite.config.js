24e45295a76d3fa010149b05b150a1412cc25298
Fabio <fabio.ramos-lopes@newmips.com>
2025-07-30 16:17:54 +0200

    try to PWA

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,42 @@ import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/Random-Choicer/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Random Choicer',
+        short_name: 'Choicer',
+        description: 'Tire au sort un élément dans une liste personnalisée',
+        theme_color: '#41B883',
+        background_color: '#181818',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        cleanupOutdatedCaches: true,
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,webmanifest}']
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
